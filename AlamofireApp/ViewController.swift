97bfa8435ddad95d35a4f8c86a3a52018247cb31
Leandro BS <joleandrobruno@gmail.com>
2022-08-02 21:48:16 -0300

    Initial Commit

---
+++ AlamofireApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireApp
+//
+//  Created by Leandro on 02/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
